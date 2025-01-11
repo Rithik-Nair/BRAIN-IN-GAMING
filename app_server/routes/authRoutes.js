e24ccae245edd39e809ed57c1ad0147e6745f869
Rithik-Nair <knrithiknair@gmal.com>
2025-01-11 11:47:00 +0530

    first commit

--- app_server/routes/authRoutes.js
+++ app_server/routes/authRoutes.js
@@ -13,4 +13,20 @@ router.get('/register', authController.registerPage);
 router.post('/login', authController.loginUser);
 router.post('/register', authController.registerUser);
 
+router.get('/mindmaze', (req, res) => {
+    res.render('mindmaze');
+});
+
+router.get('/neurodash', (req, res) => {
+    res.render('neurodash');
+});
+
+router.get('/brainbeats', (req, res) => {
+    res.render('brainbeats');
+});
+
+router.get('/cerebralcrossing', (req, res) => {
+    res.render('cerebralcrossing');
+});
+
 module.exports = router;
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -15,6 +15,22 @@ router.get('/home', authController.homePage);
 // Games page route
 router.get('/games', authController.gamesPage);
 
+router.get('/mindmaze', function(req, res, next) {
+  res.render('mindmaze', { title: 'Mind Maze' });
+});
+
+router.get('/neurodash', (req, res) => {
+  res.render('neurodash');
+});
+
+router.get('/brainbeats', (req, res) => {
+  res.render('brainbeats');
+});
+
+router.get('/cerebralcrossing', (req, res) => {
+  res.render('cerebralcrossing');
+});
+
 // Login and Register pages
 router.get('/login', authController.loginPage);
 router.get('/register', authController.registerPage);
