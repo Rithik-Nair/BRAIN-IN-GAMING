aec8d1638445d4a9dc4c4711a229e3f514072891
Rithik Nair <knrithiknair@gmail.com>
2026-01-13 22:24:36 +0530

    updated app

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Set up session middleware
 app.use(session({
-  secret: 'Br@!n_!n_G@m!ng',  // Replace with your actual secret key
+    secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }));
