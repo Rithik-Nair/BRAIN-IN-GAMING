813629cc535f6ee57c37eb7e3171e0e909c9f907
Rithik Nair <141844783+Rithik-Nair@users.noreply.github.com>
2026-01-13 22:00:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -209,7 +209,7 @@ app.use((req, res, next) => {
 });
 
 // Start the Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
